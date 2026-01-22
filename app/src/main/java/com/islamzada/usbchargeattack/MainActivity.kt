fe25b80ac5c6ec8891d2f8a44a6bb47e9fc7b991
Vusal Islamzada <vusal.islamzada.01@gmail.com>
2026-01-22 23:47:47 +0400

    Created 22.01.2026 (Vusal Islamzada)

--- app/src/main/java/com/islamzada/usbchargeattack/MainActivity.kt
+++ app/src/main/java/com/islamzada/usbchargeattack/MainActivity.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.islamzada.usbchargeattack.presentation.game.GameScreen
 import com.islamzada.usbchargeattack.ui.theme.UsbChargeAttackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             UsbChargeAttackTheme {
                 Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .windowInsetsPadding(WindowInsets.systemBars),
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Transparent
                 ) {
                     GameScreen()
                 }
